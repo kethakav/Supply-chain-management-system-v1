99f8fd0187ef89a12ec112c301bb82457a743376
Revovog <vihanga.vog@gmail.com>
2024-10-26 03:32:42 +0530

    Orders to deliver and counting Procedures Added.

---
+++ SQL/New_Procedures/SM Interface/orders_to_deliver.sql
@@ -0,0 +1,9 @@
+DELIMITER //
+
+CREATE PROCEDURE orders_to_deliver(in_store_id INT)
+BEGIN
+	SELECT * FROM orders
+	WHERE nearest_store_id = in_store_id and recieved_to_store = true and delivered_confirmation = false ;
+END //
+
+DELIMITER ;
