388d1c100f501ce269b5e4828619417761cd875a
Revovog <vihanga.vog@gmail.com>
2024-10-26 01:31:19 +0530

    Changed some queries

--- SQL/New_Procedures/FOM Interface/show_confirmed_orders.sql
+++ SQL/New_Procedures/FOM Interface/show_confirmed_orders.sql
@@ -3,7 +3,7 @@ DELIMITER //
 CREATE PROCEDURE show_confirmed_orders(in_store_id INT)
 BEGIN
 	SELECT * FROM orders 
-	WHERE confirm_payments = true and nearest_store_id = in_store_id ;
+	WHERE confirm_payments = true ;
 END //
 
 DELIMITER ;
