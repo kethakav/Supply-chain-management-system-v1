d22a014441aba0cc3ed3b4913add0e366c0bf93d
Revovog <vihanga.vog@gmail.com>
2024-10-26 01:07:54 +0530

    Recorrect remove from cart procedure.

--- SQL/New_Procedures/Customer Interface/remove_from_cart.sql
+++ SQL/New_Procedures/Customer Interface/remove_from_cart.sql
@@ -1,9 +1,9 @@
 DELIMITER //
 
-CREATE PROCEDURE `remove_from_cart`(customer_ID INT, product_ID INT)
+CREATE PROCEDURE `remove_from_cart`(in_customer_ID INT, in_product_ID INT)
 BEGIN
 	DELETE FROM cart
-    WHERE customer_id = customer_ID and product_id = product_ID ;
+    WHERE customer_id = in_customer_ID and product_id = in_product_ID ;
 END //
 
 DELIMITER ;
