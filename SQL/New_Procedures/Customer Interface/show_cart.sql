d01f9dab8eb73291de79ebe8e0681bc3b9b3023b
Kethaka Vidyananda <kethakahimath@gmail.com>
2024-10-29 12:46:44 +0530

    Updated show cart

--- SQL/New_Procedures/Customer Interface/show_cart.sql
+++ SQL/New_Procedures/Customer Interface/show_cart.sql
@@ -1,8 +1,20 @@
 DELIMITER //
 
-CREATE PROCEDURE show_cart(customer_ID INT)
+PROCEDURE `show_cart`(customer_ID INT)
 BEGIN
-	SELECT * FROM cart ;
+    SELECT 
+        c.customer_id,
+        c.product_id,
+        c.quantity,
+        p.product_name,
+        p.product_price,
+        p.product_image_url
+    FROM 
+        cart c
+    JOIN 
+        Product p ON c.product_id = p.product_id
+    WHERE 
+        c.customer_id = customer_ID;
 END //
 
 DELIMITER ;
