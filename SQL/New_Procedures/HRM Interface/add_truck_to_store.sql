be4b6aa449d6e26a2a7d12c432bf0fb38515f5f7
lihini-2002 <lihinikaduarachchilagh.22@uom.lk>
2024-10-29 18:25:47 +0530

    Delimiter added for procedure

--- SQL/New_Procedures/HRM Interface/add_truck_to_store.sql
+++ SQL/New_Procedures/HRM Interface/add_truck_to_store.sql
@@ -1,3 +1,5 @@
+DELIMITER //
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `AddTruckToStore`(
     IN tr_vehicle_number VARCHAR(20),
     IN tr_store_id INT
@@ -5,4 +7,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `AddTruckToStore`(
 BEGIN
     INSERT INTO truck (store_id, vehicle_number, availability) 
     VALUES (tr_store_id, tr_vehicle_number, 1);
-END
+END //
+
+DELIMITER ;
