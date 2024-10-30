0e8949c2375a1a1d072c18fe0c19765dd5bccc9c
Geesan Munasinghe <sugithgeesan@gmail.com>
2024-10-30 09:56:18 +0530

    Minor procedure fix

--- SQL/New_Procedures/all_new_pros.sql
+++ SQL/New_Procedures/all_new_pros.sql
@@ -609,7 +609,7 @@ CREATE PROCEDURE GetAstDriversByStoreID(IN input_store_id INT)
 BEGIN
     SELECT 
         d.ast_driver_id,
-        d.ast_user_name,
+        d.user_name,
         d.ast_driver_first_name,
         d.ast_driver_last_name,
         d.ast_driver_contact,
