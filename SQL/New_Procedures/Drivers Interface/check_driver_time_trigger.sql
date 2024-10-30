73ea3fa79eb512e0635f99fd235574c40d676a79
Kethaka Vidyananda <kethakahimath@gmail.com>
2024-10-30 19:44:42 +0530

    gg

--- SQL/New_Procedures/Drivers Interface/check_driver_time_trigger.sql
+++ SQL/New_Procedures/Drivers Interface/check_driver_time_trigger.sql
@@ -1,27 +1,30 @@
 DELIMITER //
 
 CREATE TRIGGER check_driver_time
-BEFORE INSERT ON truck_delivery
+BEFORE INSERT ON Truck_Delivery
 FOR EACH ROW
 BEGIN 
     DECLARE driver_time INT DEFAULT 0;
     DECLARE ast_driver_time INT DEFAULT 0;
     DECLARE route_time INT DEFAULT 0;
 
+    -- Get the driver’s worked hours
     SELECT worked_hours INTO driver_time 
-    FROM driver
+    FROM Driver
     WHERE driver_id = NEW.driver_id;
 
+    -- Get the assistant driver’s worked hours
     SELECT worked_hours INTO ast_driver_time
-    FROM assistant_driver
+    FROM Assistant_Driver
     WHERE ast_driver_id = NEW.ast_driver_id;
     
-    SELECT max_time INTO ast_driver_time
-    FROM route
+    -- Get the route max time
+    SELECT max_time INTO route_time
+    FROM Route
     WHERE route_id = NEW.route_id;
     
-
-    IF (driver_time < 40 OR ast_driver_time < 60) THEN
+    -- Check if driver or assistant driver exceeds max allowed time
+    IF ((driver_time + route_time) > 40 OR (ast_driver_time + route_time) > 60) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Driver or Assistant Driver Limit Exceeded.';
     END IF;
