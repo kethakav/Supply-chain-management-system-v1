be4b264a9a7c25d8f7f75d89d41f04d0e8a70eec
kalawilapathiragepasindumanujaya <manujaya.22@cse.mrt.ac.lk>
2024-10-30 11:02:04 +0530

    breadcrumbs for customers

--- src/app/dashboard/customer/history/page.tsx
+++ src/app/dashboard/customer/history/page.tsx
@@ -3,6 +3,7 @@
 import { Metadata } from "next";
 import DashboardLayout from "@/components/Layouts/DashboardLayout";
 import React, { useEffect, useState } from "react";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 
 interface Order {
   order_id: string;
@@ -57,6 +58,7 @@ export default function Home() {
   return (
     <>
       <DashboardLayout>
+      <Breadcrumb pageName="History" />
         <div className="rounded-[10px] bg-white shadow-1 dark:bg-gray-dark dark:shadow-card">
           <div className="px-4 py-6 md:px-6 xl:px-9">
             <h4 className="text-body-2xlg font-bold text-dark dark:text-white">
